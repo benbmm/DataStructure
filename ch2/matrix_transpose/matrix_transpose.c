724dd3653ae7c854b234a7aefa92b2711d52cf83
benbmm <tsaiben321@gmail.com>
2025-03-24 16:53:14 +0800

    a

---
+++ ch2/matrix_transpose/matrix_transpose.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_TERMS 100
+typedef struct{
+    int col;
+    int row;
+    int val;
+}mat;
+mat a[MAX_TERMS];
+mat b[MAX_TERMS];
+
+
+
+void trans(mat a,mat b){
+    
+}
